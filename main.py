0361507d7a34ccaa30da53732c4e3d34cfa80683
Allan Oliveira <allanoliveira.dev@gmail.com>
2024-10-05 01:36:29 -0300

    completar a prova da aula 1

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Bem vindo a calculadora de area de triangulos")
+base = float(input("Digite a medida da base do triangulo em centimetros: "))
+altura = float(input("Agora digite a altura do triangulo em centimetros: "))
+
+print(f"O seu triangulo tem a area de {base * altura} centimetros ")
